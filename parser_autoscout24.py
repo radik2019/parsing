471a1d9b4e55ce8e5b00e43061ef6b5dd6f329f5
radik <codefacs@gmail.com>
2021-04-26 12:47:19 +0200

    scrapping recursivley

--- parser_autoscout24.py
+++ parser_autoscout24.py
@@ -6,6 +6,15 @@ import pandas as pd
 import re
 
 
+HOST = "https://www.autoscout24.it"
+
+dict_data = {
+    'Tipo di veicolo': [], 'Marca': [], 'Modello': [], 'Anno': [],
+    'Colore esterno': [], 'Tipo di vernice': [], 'Carrozzeria': [],
+    'Porte': [], 'Posti a sedere': [], 'Tipo di cambio': [], 'Marce': [],
+    'Cilindrata': [], 'Cilindri': [], 'Peso a vuoto': [], 'Alimentazione': [],
+    'Classe emissioni': [], 'Per neopatentati': [], 'Consumo': [], 'Emissioni CO2': []
+}
 
 
 url = "https://www.autoscout24.it/lst/ford?sort=standard&desc=0&ustate=N%2CU&cy=I&atype=C"
@@ -15,11 +24,15 @@ HEADERS = {
     'user_agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85'
                   'Safari/537.36 '
 }
+EMISSIONI_CO2 = 'Emissioni di CO2I dati di consumi ed emissioni per le auto usate si intendono riferiti al ciclo NEDC. Per le auto nuove, a partire dal 16.2.2021, iI rivenditore deve indicare i valori relativi al consumo di carburante ed emissione di CO2 misurati con il ciclo WLTP. Il rivenditore deve rendere disponibile nel punto vendita una guida gratuita su risparmio di carburante e emissioni di CO2 dei nuovi modelli di autovetture. Anche stile di guida e altri fattori non tecnici influiscono su consumo di carburante e emissioni di CO2. Il CO2 è il gas a effetto serra principalmente responsabile del riscaldamento terrestre.'
+CONSUMO = 'Consumo carburante:I dati di consumi ed emissioni per le auto usate si intendono riferiti al ciclo NEDC. Per le auto nuove, a partire dal 16.2.2021, iI rivenditore deve indicare i valori relativi al consumo di carburante ed emissione di CO2 misurati con il ciclo WLTP. Il rivenditore deve rendere disponibile nel punto vendita una guida gratuita su risparmio di carburante e emissioni di CO2 dei nuovi modelli di autovetture. Anche stile di guida e altri fattori non tecnici influiscono su consumo di carburante e emissioni di CO2. Il CO2 è il gas a effetto serra principalmente responsabile del riscaldamento terrestre.'
+
+CONSUMO_ELETTRICO = 'Consumo di energia elettricaI dati di consumi ed emissioni per le auto usate si intendono riferiti al ciclo NEDC. Per le auto nuove, a partire dal 16.2.2021, iI rivenditore deve indicare i valori relativi al consumo di carburante ed emissione di CO2 misurati con il ciclo WLTP. Il rivenditore deve rendere disponibile nel punto vendita una guida gratuita su risparmio di carburante e emissioni di CO2 dei nuovi modelli di autovetture. Anche stile di guida e altri fattori non tecnici influiscono su consumo di carburante e emissioni di CO2. Il CO2 è il gas a effetto serra principalmente responsabile del riscaldamento terrestre.'
 
 
 def transform_to_float(a):
     """elabora i dati dei consumi"""
-    
+
     a = a.split()[0].replace(",", ".")
     if a == '-/-':
         return 0
@@ -33,7 +46,7 @@ def get_html(url, params=''):
 
 
 def engine_power(s):
-    
+
     pattern = r'\d+'
     n = [int(i) for i in re.findall(pattern, s)]
     if len(n) != 2:
@@ -48,16 +61,17 @@ def price_processing(s):
 
 
 def km_average(a):
-    a = a.split()[0].replace('.','')
+    a = a.split()[0].replace('.', '')
     if a == '-':
         return 0
     return int(a)
-    
 
-def get_content2(url):
+
+def get_csv_file(url):
     html = get_html(url)
     parser = BeautifulSoup(html, 'html.parser')
-    items = parser.find_all('div', class_="cl-list-element cl-list-element-gap")
+    items = parser.find_all(
+        'div', class_="cl-list-element cl-list-element-gap")
     data = {
         'kw': [],
         'cv': [],
@@ -69,64 +83,105 @@ def get_content2(url):
         'immatricolazione': [],
         'consumo': [],
         'link': []
-        }
+    }
+    data2 = {}
     for i in items:
-        dati_diversi = [item for item in i.find('div', class_="cldt-summary-vehicle-data").get_text().split("\n") if item != '']
-
-        data['price Euro'].append(price_processing(i.find(class_="cldt-price sc-font-xl sc-font-bold").get_text().split('\n')[1]))
-        data['marchio'].append(i.find(class_="cldt-summary-makemodel sc-font-bold sc-ellipsis").get_text())
+        dati_diversi = [item for item in i.find(
+            'div', class_="cldt-summary-vehicle-data").get_text().split("\n") if item != '']
+        data['price Euro'].append(price_processing(
+            i.find(class_="cldt-price sc-font-xl sc-font-bold").get_text().split('\n')[1]))
+        data['marchio'].append(
+            i.find(class_="cldt-summary-makemodel sc-font-bold sc-ellipsis").get_text())
         data['motore'].append(dati_diversi[6])
         data['kilometri'].append(km_average(dati_diversi[0]))
         data['kw'].append(engine_power(dati_diversi[2])[0])
         data['cv'].append(engine_power(dati_diversi[2])[1])
         data['immatricolazione'].append(dati_diversi[1])
         data['consumo'].append(transform_to_float(dati_diversi[7]))
-        data['link'].append("https://www.autoscout24.it" +i.find('a').get("href"))
-    df = pd.DataFrame(data)
+        data_dict = get_data_from_link("https://www.autoscout24.it" + i.find('a').get("href"))
+        for i in data_dict:
+            try:
+                data[i].append(data_dict[i])
+            except KeyError:
+                data[i] = [data_dict[i]]
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df = df.transpose()
     return df
 
 
-def get_len(html):
-    html = get_html(url)
+def get_data_from_link(link: str) -> dict:
+    global dict_data
+    dict_data = {}
+    html = get_html(link)
     parser = BeautifulSoup(html, 'html.parser')
-    items = parser.find_all("a", href="?&sort=standard&desc=0&ustate=N%2CU&size=20&cy=I&atype=C&page")
+    items = parser.find_all("div", class_='sc-grid-row')
     for i in items:
-        print(i)
+        a = i.find('dd')
+        if i.find('dd') != None:
+
+            list_data = [d.get_text().strip() for d in i.find_all("dd")]
+            list_title = [k.get_text() for k in i.find_all("dt")]
+            for i in range(len(list_title)):
+                if list_title[i] == EMISSIONI_CO2:
+                    list_title[i] = 'Emissioni_CO2'
+                elif list_title[i] == CONSUMO:
+                    list_title[i] = 'Consumo'
+                elif list_title[i] == CONSUMO_ELETTRICO:
+                    list_title[i] = 'Consumo elettrico'
+            for item in range(len(list_title)):
+                dict_data[list_title[item]] = list_data[item]
+            return dict_data
 
 
 def run():
     car = input('macchina:  ')
-
     lnk = f"https://www.autoscout24.it/lst/{car}?sort=standard&desc=0&ustate=N%2CU&size=20&page="
     df = pd.DataFrame({
-            'price Euro': [],
-            'marchio': [],
-            'motore': [],
-            'kilometri': [],
-            'kw': [],
-            'cv': [],
-            'immatricolazione': [],
-            'consumo': [],
-            'link': []
-            })
+        'price Euro': [],
+        'marchio': [],
+        'motore': [],
+        'kilometri': [],
+        'kw': [],
+        'cv': [],
+        'immatricolazione': [],
+        'consumo': [],
+        'link': []
+    })
     for i in range(1, 21):
         print(f'parse page {i}')
-        # lst2 += get_content2(lnk + str(i))
-        df = pd.concat([df, get_content2(lnk + str(i))], ignore_index=True)
 
+        df = pd.concat([df, get_csv_file(lnk + str(i))], ignore_index=True)
     car = '_'.join(car.split("/"))
     df.to_csv(f"{car}.csv")
-# print(df)
-# dg = get_content2(url)
-# print(dg,22)
+
+
+if __name__ == '__main__':
+    run()
 
 
 
-"""
-https://www.autoscout24.it/lst/citroen?&sort=standard&desc=0&ustate=N%2CU&size=20&atype=C&page=2
-?&sort=standard&desc=0&ustate=N%2CU&size=20&cy=I&atype=C&page=
-"?&amp;sort=standard&amp;desc=0&amp;ustate=N%2CU&amp;size=20&amp;atype=C&amp;page="
-https://www.autoscout24.it/lst/citroen/berlingo?sort=standard&desc=0&ustate=N%2CU&size=20&page=1&atype=C&fc=2&qry=&
-https://www.autoscout24.it/lst/audi?sort=price&desc=0&superdeal=true&ustate=N%2CU&size=20&page=1&cy=I&atype=C&fc=8&qry=&
-https://www.autoscout24.it/lst/bmw?sort=standard&desc=0&ustate=N%2CU&size=20&page=1&atype=C&fc=1&qry=&
-"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
