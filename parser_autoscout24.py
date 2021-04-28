819006e3b31376b0ec7c8fc564d97d0dc5bef299
radik <codefacs@gmail.com>
2021-04-28 11:17:32 +0200

    dict of data now is a global variable

--- parser_autoscout24.py
+++ parser_autoscout24.py
@@ -5,18 +5,10 @@ import requests
 import pandas as pd
 import re
 import multiprocessing
-
+import threading
+lock = multiprocessing.Lock()
 
 HOST = "https://www.autoscout24.it"
-
-dict_data = {
-    'Tipo di veicolo': [], 'Marca': [], 'Modello': [], 'Anno': [],
-    'Colore esterno': [], 'Tipo di vernice': [], 'Carrozzeria': [],
-    'Porte': [], 'Posti a sedere': [], 'Tipo di cambio': [], 'Marce': [],
-    'Cilindrata': [], 'Cilindri': [], 'Peso a vuoto': [], 'Alimentazione': [],
-    'Classe emissioni': [], 'Per neopatentati': [], 'Consumo': [], 'Emissioni CO2': []
-}
-
 
 url = "https://www.autoscout24.it/lst/ford?sort=standard&desc=0&ustate=N%2CU&cy=I&atype=C"
 
@@ -42,8 +34,6 @@ data = {
     'consumo': [],
     'link': []
 }
-data2 = {}
-
 
 def transform_to_float(a):
     """elabora i dati dei consumi"""
@@ -83,13 +73,15 @@ def km_average(a):
 
 
 def get_csv_file(url, number_thread):
-    print(f"runing parsing page {number_thread}")
+
     global data
+    print(f"runing parsing page {number_thread}")
     html = get_html(url)
     parser = BeautifulSoup(html, 'html.parser')
     items = parser.find_all(
         'div', class_="cl-list-element cl-list-element-gap")
-    
+
+    # lock.acquire()
     for i in items:
         dati_diversi = [item for item in i.find(
             'div', class_="cldt-summary-vehicle-data").get_text().split("\n") if item != '']
@@ -103,27 +95,25 @@ def get_csv_file(url, number_thread):
         data['cv'].append(engine_power(dati_diversi[2])[1])
         data['immatricolazione'].append(dati_diversi[1])
         data['consumo'].append(transform_to_float(dati_diversi[7]))
-        data_dict = get_data_from_link("https://www.autoscout24.it" + i.find('a').get("href"))
-        for i in data_dict:
-            try:
-                data[i].append(data_dict[i])
-            except KeyError:
-                data[i] = [data_dict[i]]
+        # lock.release()
+        get_data_from_link("https://www.autoscout24.it" + i.find('a').get("href"))
+
+    
     print(f"finishing parsing page {number_thread}")
-    # df = pd.DataFrame.from_dict(data, orient='index')
-    # df = df.transpose()
-    # return df
+
 
 def get_data_from_link(link: str) -> dict:
-    global dict_data
-    dict_data = {}
+    global data
     html = get_html(link)
     parser = BeautifulSoup(html, 'html.parser')
     items = parser.find_all("div", class_='sc-grid-row')
+    
+    # lock.acquire()
+
+
     for i in items:
         a = i.find('dd')
         if i.find('dd') != None:
-
             list_data = [d.get_text().strip() for d in i.find_all("dd")]
             list_title = [k.get_text() for k in i.find_all("dt")]
             for i in range(len(list_title)):
@@ -134,45 +124,95 @@ def get_data_from_link(link: str) -> dict:
                 elif list_title[i] == CONSUMO_ELETTRICO:
                     list_title[i] = 'Consumo elettrico'
             for item in range(len(list_title)):
-                dict_data[list_title[item]] = list_data[item]
-            return dict_data
+                try:
+                    data[list_title[item]].append(list_data[item])
+                except KeyError:
+                    data[list_title[item]] = [list_data[item]]
 
-def run():
-    car = input('macchina:  ')
+            # lock.release()
+            return
+
+def run_proc(car):
+
     lnk = f"https://www.autoscout24.it/lst/{car}?sort=standard&desc=0&ustate=N%2CU&size=20&page="
-    df = pd.DataFrame({
-        'price Euro': [],
-        'marchio': [],
-        'motore': [],
-        'kilometri': [],
-        'kw': [],
-        'cv': [],
-        'immatricolazione': [],
-        'consumo': [],
-        'link': []
-    })
-
     thr = []
-    for i in range(1, 21):
-
+    for i in range(1, 10):
+        # get_csv_file(lnk + str(i),i)
         t = multiprocessing.Process(target=get_csv_file, args=(lnk + str(i),i))
         thr.append(t)
         t.start()
     for k in range(len(thr)):
+
         thr[k].join()
-    
+
+    print(len(data))
     df2 = pd.DataFrame.from_dict(data, orient='index')
     df2 = df2.transpose()
-
-    df = pd.concat([df, df2], ignore_index=True)
-
     car = '_'.join(car.split("/"))
-    df.to_csv(f"{car}.csv")
+    df2.to_csv(f"{car}.csv")
+
+
+def run_thr(car):
+
+    lnk = f"https://www.autoscout24.it/lst/{car}?sort=standard&desc=0&ustate=N%2CU&size=20&page="
+    thr = []
+    for i in range(1, 10):
+        # get_csv_file(lnk + str(i),i)
+        t = threading.Thread(target=get_csv_file, args=(lnk + str(i),i))
+        thr.append(t)
+        t.start()
+    for k in range(len(thr)):
+
+        thr[k].join()
+    
+    print(len(data))
+    df2 = pd.DataFrame.from_dict(data, orient='index')
+    df2 = df2.transpose()
+    car = '_'.join(car.split("/"))
+    df2.to_csv(f"{car}.csv")
+
+car = input('macchina:  ')
+
+def run(car):
+
+    lnk = f"https://www.autoscout24.it/lst/{car}?sort=standard&desc=0&ustate=N%2CU&size=20&page="
+    thr = []
+    for i in range(1, 10):
+        get_csv_file(lnk + str(i),i)
+    
+    print(len(data))
+    df2 = pd.DataFrame.from_dict(data, orient='index')
+    df2 = df2.transpose()
+    car = '_'.join(car.split("/"))
+    df2.to_csv(f"{car}.csv")
+
 
 
 if __name__ == '__main__':
     import time
     tmp = time.time()
-    run()
-    print(time.time() - tmp)
+    run_thr(car)
+    print('      threading  ',time.time() - tmp, len(data['marchio']))
+    tmp = 0
 
+    data = {
+        'kw': [],
+        'cv': [],
+        'marchio': [],
+        'price Euro': [],
+        'marchio': [],
+        'motore': [],
+        'kilometri': [],
+        'immatricolazione': [],
+        'consumo': [],
+        'link': []
+    }
+    tmp = time.time()
+    run_proc(car)
+    print('      processing  ',time.time() - tmp, len(data['marchio']))
+    tmp = 0
+
+    tmp = time.time()
+    run(car)
+    print('      normal  ',time.time() - tmp, len(data['marchio']))
+    tmp = 0
